f226c5e5d896bd0f8000d9be8e23d7c423f55cd9
Kakada <kakadanoeng8@gmail.com>
2026-06-02 09:36:39 +0700

    upload

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import logging
 
 import asyncio
 
-# Direct absolute lookups to local directory 1
+# Direct absolute lookups to local directory 12
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
